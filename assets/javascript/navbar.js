8d4f32f749051c4edda19412193b6cbbc6c98cb5
ChivSuroo <sdhughes1998@gmail.com>
2022-04-13 12:11:33 +0100

    - Added Image Slow Homepage

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -10,3 +10,18 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 9000); // Change image every 2 seconds
+}
